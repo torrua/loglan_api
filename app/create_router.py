7d1b2f57664c58439e933fab2975c922ce350dff
torrua <torrua@users.noreply.github.com>
2024-09-20 09:12:35 +0700

    DRAFT update create router

--- app/create_router.py
+++ app/create_router.py
@@ -1,20 +1,26 @@
 from __future__ import annotations
 
-from typing import Callable, List
+from typing import TypeVar, Callable, List, Optional
 
-from fastapi import HTTPException, APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from loglan_core import Base as BaseORMModel
 from loglan_core.addons.base_selector import BaseSelector
+from sqlalchemy import Row
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.inspection import inspect
 from starlette.requests import Request
 
 from app.engine import get_db
+from app.models import Base
+
+T = TypeVar("T", bound=BaseORMModel)
 
 
 def create_router(
-    orm_model,
-    base_response_model,
-    detailed_response_model,
-    validate_func: Callable | None = None,
+    orm_model: T,
+    base_response_model: Base,
+    detailed_response_model: Base,
+    validate_func: Optional[Callable[[Base, Row[T]], None]] = None,
 ) -> APIRouter:
     router = APIRouter(
         prefix=f"/api/v2/{orm_model.__tablename__.lower()}",
@@ -40,6 +46,11 @@ def create_router(
         db: AsyncSession = Depends(get_db),
     ) -> detailed_response_model:
 
+        orm_fields = sorted(column.name for column in inspect(orm_model).c)
+
+        await validate_query_params(request, orm_fields)
+
+        # Check for the required 'id' parameter
         if id_param := request.query_params.get("id", None):
             id_ = int(id_param)
         else:
@@ -64,7 +75,28 @@ def create_router(
         response = detailed_response_model.model_validate(item)
 
         if validate_func:
-            await validate_func(response, item)
+            validate_func(response, item)
         return response
+
+    async def validate_query_params(request: Request, orm_fields: List[str]) -> None:
+        """
+        Validate the query parameters
+
+        Args:
+            request: The request object
+            orm_fields: The list of ORM fields
+
+        Raises:
+            HTTPException: If the query parameter is not in the list of ORM fields
+        """
+        for param in request.query_params.keys():
+            if param not in orm_fields:
+                raise HTTPException(
+                    status_code=400,
+                    detail=(
+                        f"Invalid query parameter: {param}. "
+                        f"Allowed parameters are: {', '.join(orm_fields)}"
+                    ),
+                )
 
     return router
